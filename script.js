fd6b1fe91e5e0ac07f69def7fbb9e6ef2f540964
subal <subalbarik7780@gmail.com>
2024-10-17 14:16:52 +0530

    add 1st js file

---
+++ script.js
@@ -0,0 +1,13 @@
+function generatePassword() {
+    var length = 10; 
+    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+    var password = "";
+    
+    for (var i = 0; i < length; i++) {
+        var randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    
+    
+      const show = document.getElementById("password").textContent = password;
+}
